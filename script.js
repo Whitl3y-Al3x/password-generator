59217bb335bd0185e7bbf688aba70088ed18e868
Whitley Alexander <whitleyalexander@gmail.com>
2021-10-27 16:47:58 -0500

    initial project

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
 
 // Password length between 8-128.
 // Lowercase, uppercase, numbers, special characters.
-const alpha = 'abcdefghijklmnopqrstuvwxyz';
-const calpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const num = '1234567890';
 const specials = ',.!@#$%^&*';
-const options = [alpha, alpha, alpha, calpha, calpha, num, num, specials];
+const options = [lower, lower, lower, upper, upper, num, num, specials];
+
+function generatePassword () {
+
+let passwordChar = "";
+// Create user prompt to select password length
+let passwordLengthUser = window.prompt("How many characters would you like your password to be? Password must be between 8-128 characters.");
+passwordLengthUser = parseInt(passwordLengthUser);
+
+if(passwordLengthUser < 8) {
+  alert("Password must have more than 7 characters!");
+  return "";
+};
+
+if (passwordLengthUser > 128) {
+  alert("Password must not have more than 128 characters!");
+  return "";
+};
+
+// Creates confirm boolean for lowercase "yes or no"
+let lower = window.confirm("Would you like to add lowercase characters?");
+
+if (lower) {
+  passwordChar += options.lower;
+};
+
+// Create confrim boolenan for uppercase "yes or no"
+let upper = window.confirm("Would you like to add uppercase characters?");
+
+if (upper) {
+  passwordChar += options.upper;
+};
+
+// Create confirm boolean for numbers "yes or no"
+let num = window.confirm("Would you like to add numbers?");
+
+if (num) {
+  passwordChar += options.num;
+};
+
+// Create confirm boolean for special characters "yes or no"
+let specials = window.confirm("Would you like to add special characters?");
+
+if(specials) {
+  passwordChar += options.specials;
+};
+
 let opt, choose;
 let password = "";
 for ( let i = 0; i < 8; i++ ) {
